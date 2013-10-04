a45f172fa72296fbcbd69b4b4055befb0e8ab44d
Callum Stott <callum@seadowg.com>
2013-10-03 19:33:00 -0700

    FRP play button control

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'shoes'
 gem 'pebblewatch'
+gem 'frappuccino'
---
+++ pebble-tool.rb
@@ -0,0 +1,33 @@
+require "pebble"
+require "frappuccino"
+
+module PebbleTool
+  class Watch < Pebble::Watch
+    def play_button
+      button_event(:playpause)
+    end
+    
+    private
+    
+    def button_event(button_name)
+      proxy = Event.new
+      
+      on_event(:media_control) do |event|
+        case event.button
+        when button_name
+          proxy.occur
+        end
+      end
+      
+      Frappuccino::Stream.new(proxy)
+    end
+  end
+  
+  private
+  
+  class Event
+    def occur
+      emit(nil)
+    end
+  end
+end
