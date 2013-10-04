78bddc989712be55f4f18da12a65bf2757f19c2b
Callum Stott <callum@seadowg.com>
2013-10-03 20:22:05 -0700

    Very basic lithium hunter

---
+++ examples/lithium-hunter.rb
@@ -0,0 +1,35 @@
+require_relative '../pebble-tool'
+
+class Playing
+  attr_reader :count
+  
+  def intialize(count)
+    @count = count
+  end
+end
+
+class Lose; end
+class Win; end
+
+puts "Wait..."
+
+watch = PebbleTool::Watch.autodetect
+watch.connect
+puts "Connected..."
+
+controller = watch.play_button.scan(0) do |accum, current|
+  accum = 0 if accum > 468
+  accum + (Random.new.rand * 20).ceil
+end
+
+game = controller.on_value do |count|
+  if count < 468
+    puts "You have #{count} lithiums..."
+  elsif count > 468
+    puts "You lose... Try again!"
+  else
+    puts "You win!... but still... Try again!"
+  end
+end
+
+watch.listen_for_events
